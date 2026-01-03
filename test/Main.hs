af6fabf4c6c1f25403f88a0789a2d074dce07a1f
Daniel Díaz <diaz_carrete@yahoo.com>
2026-01-03 20:18:10 +0100

    trouble coercing

--- test/Main.hs
+++ test/Main.hs
@@ -12,34 +12,36 @@ import GHC.Records
 import Optics.Core
 import Optics.Dot
 
-data Whole a = Whole
+data Whole z = Whole
   { whole1 :: Int,
-    part :: Part a
+    part :: Part z
   }
   deriving stock (Generic, Show)
-  deriving (RecordDotOptics name a b (Whole p) (Whole q)) via (GenericDotOptics (Whole q))
+  deriving (RecordDotOptics name a b (Whole p)) via (GenericDotOptics (Whole z))
 
 -- instance (GField name (Whole p) (Whole q) a b) => RecordDotOptics name  a b (Whole q) (Whole p) where
 --   dotOptic = gfield @name
 
-data Part a = Part
+data Part z = Part
   { part1 :: Bool,
-    subpart :: Subpart a
+    subpart :: Subpart z
   }
   deriving stock (Generic, Show)
+  deriving (RecordDotOptics name a b (Part p))  via (GenericDotOptics (Part z))
 
-instance (GField name (Part p) (Part q) a b) => RecordDotOptics name a b (Part q) (Part p) where
-  dotOptic = gfield @name
+-- instance (GField name (Part p) (Part q) a b) => RecordDotOptics name a b (Part q) (Part p) where
+--   dotOptic = gfield @name
 
-data Subpart a = Subpart
+data Subpart z = Subpart
   { wee :: String,
-    foo :: a,
+    foo :: z,
     yet :: YetAnotherSubpart
   }
   deriving stock (Generic, Show)
+  deriving (RecordDotOptics name a b (Subpart p))  via (GenericDotOptics (Subpart z))
 
-instance (GField name (Subpart p) (Subpart q) a b) => RecordDotOptics name a b (Subpart q) (Subpart p)  where
-  dotOptic = gfield @name
+-- instance (GField name (Subpart p) (Subpart q) a b) => RecordDotOptics name a b (Subpart q) (Subpart p)  where
+--   dotOptic = gfield @name
 
 data YetAnotherSubpart = YetAnotherSubpart
   { ooo :: String,
@@ -47,17 +49,17 @@ data YetAnotherSubpart = YetAnotherSubpart
   }
   deriving (Show)
 
--- | 'YetAnotherSubpart' doesn't use the 'GField' machinery for
--- 'RecordDotOptics'. Instead, it uses 'HasField'/'SetField'. Field-changing
--- updates are not supported here.
-instance
-  (HasField name YetAnotherSubpart x, SetField name YetAnotherSubpart x) =>
-  RecordDotOptics name x x YetAnotherSubpart YetAnotherSubpart
-  where
-  dotOptic = Optics.Core.lens (getField @name) (flip (setField @name))
-
-instance SetField "ooo" YetAnotherSubpart String where
-  setField ooo r = r {ooo}
+-- -- | 'YetAnotherSubpart' doesn't use the 'GField' machinery for
+-- -- 'RecordDotOptics'. Instead, it uses 'HasField'/'SetField'. Field-changing
+-- -- updates are not supported here.
+-- instance
+--   (HasField name YetAnotherSubpart x, SetField name YetAnotherSubpart x) =>
+--   RecordDotOptics name x x YetAnotherSubpart YetAnotherSubpart
+--   where
+--   dotOptic = Optics.Core.lens (getField @name) (flip (setField @name))
+-- 
+-- instance SetField "ooo" YetAnotherSubpart String where
+--   setField ooo r = r {ooo}
 
 whole :: Whole Int
 whole = Whole 0 (Part True (Subpart "wee" 7 (YetAnotherSubpart "oldval" 3)))
@@ -66,9 +68,9 @@ whole = Whole 0 (Part True (Subpart "wee" 7 (YetAnotherSubpart "oldval" 3)))
 whole' :: Whole Bool
 whole' = whole & the.part.subpart.foo .~ False
 
--- | Non-type changed update which includes 'GField' lenses and 'HasField'/'SetField' lenses.
-whole'' :: Whole Int
-whole'' = whole & the.part.subpart.yet.ooo .~ "newval"
+-- -- | Non-type changed update which includes 'GField' lenses and 'HasField'/'SetField' lenses.
+-- whole'' :: Whole Int
+-- whole'' = whole & the.part.subpart.yet.ooo .~ "newval"
 
 main :: IO ()
 main = do
