8610fcee2ddbc25b4d2ee2a72eb40f34ca787c10
Daniel Díaz <diaz_carrete@yahoo.com>
2026-01-03 17:07:46 +0100

    Example of optics obtained two ways.

--- lib/Optics/Lens/Dot.hs
+++ lib/Optics/Lens/Dot.hs
@@ -1,51 +1,31 @@
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE RequiredTypeArguments #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Optics.Lens.Dot where
 
+import Data.Kind
 import GHC.Records
 import Optics.Core
-import Optics.Coerce
-import GHC.TypeLits
-import Data.Proxy
-import Data.Kind
-    
-instance 
-    (RecordDotOptics name u v a b,
-     JoinKinds k A_Lens m, 
-     AppendIndices is NoIx ks)
-    =>
-    HasField name (Optic k is s t u v) (Optic m ks s t a b) where
-        getField o = o % theOptic @name
+
+instance
+  ( RecordDotOptics name u v a b,
+    JoinKinds k A_Lens m,
+    AppendIndices is NoIx ks
+  ) =>
+  HasField name (Optic k is s t u v) (Optic m ks s t a b)
+  where
+  getField o = o % dotOptic @name
 
 the :: Iso a b a b
 the = Optics.Core.equality
 
 class RecordDotOptics name u v a b | name u -> v a b, name v -> u a b where
-    theOptic :: Lens u v a b
+  dotOptic :: Lens u v a b
 
--- newtype GenericOptics r = GenericOptics r
-
--- https://stackoverflow.com/questions/53009549/haskell-derivingvia-on-multi-param-type-classes-with-fun-deps
-
--- instance GField name u v a b => RecordDotOptics name (GenericOptics u) v a b where
---     theOptic _ = Optics.Coerce.coerceS (gfield @name)
-
--- instance 
---     GField (name :: Symbol) a a b b
---     =>
---     HasField name (Optic A_Lens NoIx s s a a) (Optic A_Lens NoIx s s b b) where
---         getField theLens = theLens %% gfield @name 
---     
-
--- instance 
---     GField (name :: Symbol) u v a b
---     =>
---     HasField name (Optic A_Lens NoIx s t u v) (Optic A_Lens NoIx s t a b) where
---         getField = (% gfield @name)
-
-type SetField :: forall {k} . k -> Type -> Type -> Constraint
+-- | This should be in base in the future.
+type SetField :: forall {k}. k -> Type -> Type -> Constraint
 class SetField x r a | x r -> a where
   -- | Selector function to extract the field from the record.
   setField :: a -> r -> r
--- test/Main.hs
+++ test/Main.hs
@@ -1,17 +1,15 @@
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE NoFieldSelectors #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE NoFieldSelectors #-}
 
 -- https://stackoverflow.com/questions/53009549/haskell-derivingvia-on-multi-param-type-classes-with-fun-deps
 module Main (main) where
 
-import Optics.Coerce
-import GHC.TypeLits
 import GHC.Generics
+import GHC.Records
 import Optics.Core
 import Optics.Lens.Dot
-import GHC.Records
 
 data Whole a = Whole
   { whole1 :: Int,
@@ -19,8 +17,8 @@ data Whole a = Whole
   }
   deriving stock (Generic, Show)
 
-instance GField name (Whole p) (Whole q) a b => RecordDotOptics name (Whole p) (Whole q) a b where
-    theOptic = gfield @name
+instance (GField name (Whole p) (Whole q) a b) => RecordDotOptics name (Whole p) (Whole q) a b where
+  dotOptic = gfield @name
 
 data Part a = Part
   { part1 :: Bool,
@@ -28,39 +26,50 @@ data Part a = Part
   }
   deriving stock (Generic, Show)
 
-instance GField name (Part p) (Part q) a b => RecordDotOptics name (Part p) (Part q) a b where
-    theOptic = gfield @name
+instance (GField name (Part p) (Part q) a b) => RecordDotOptics name (Part p) (Part q) a b where
+  dotOptic = gfield @name
 
 data Subpart a = Subpart
-  { subpart1 :: String,
+  { wee :: String,
     foo :: a,
-    subpart2 :: YetAnotherSubpart
+    yet :: YetAnotherSubpart
   }
   deriving stock (Generic, Show)
 
-instance GField name (Subpart p) (Subpart q) a b => RecordDotOptics name (Subpart p) (Subpart q) a b where
-    theOptic = gfield @name
+instance (GField name (Subpart p) (Subpart q) a b) => RecordDotOptics name (Subpart p) (Subpart q) a b where
+  dotOptic = gfield @name
 
-data YetAnotherSubpart = YetAnotherSubpart  {
-        wee :: Int,
-        bar :: Int
-    }
-    deriving Show
+data YetAnotherSubpart = YetAnotherSubpart
+  { ooo :: String,
+    uuu :: Int
+  }
+  deriving (Show)
 
-instance (HasField name YetAnotherSubpart x, SetField name YetAnotherSubpart x)  =>
-    RecordDotOptics name (Subpart a) (Subpart a) x x where
-    theOptic = Optics.Core.lens (getField @name) (flip (setField @name))
+-- | 'YetAnotherSubpart' doesn't use the 'GField' machinery for
+-- 'RecordDotOptics'. Instead, it uses 'HasField'/'SetField'. Field-changing
+-- updates are not supported here.
+instance
+  (HasField name YetAnotherSubpart x, SetField name YetAnotherSubpart x) =>
+  RecordDotOptics name YetAnotherSubpart YetAnotherSubpart x x
+  where
+  dotOptic = Optics.Core.lens (getField @name) (flip (setField @name))
 
-instance SetField "wee" YetAnotherSubpart Int where
-    setField wee r = r { wee }
+instance SetField "ooo" YetAnotherSubpart String where
+  setField ooo r = r {ooo}
 
 whole :: Whole Int
-whole = Whole 0 (Part True (Subpart "wee" 7 (YetAnotherSubpart 2 3)) )
+whole = Whole 0 (Part True (Subpart "wee" 7 (YetAnotherSubpart "oldval" 3)))
 
+-- | Note the the type-changing update
 whole' :: Whole Bool
 whole' = whole & the.part.subpart.foo .~ False
 
+-- | Non-type changed update which includes 'GField' lenses and 'HasField'/'SetField' lenses.
+whole'' :: Whole Int
+whole'' = whole & the.part.subpart.yet.ooo .~ "newval"
+
 main :: IO ()
-main = do 
-    print whole
-    print whole'
+main = do
+  print whole
+  print whole'
+  print whole''
