88b2aac0a3851f20394f150501faf1247fb29122
Daniel Díaz <diaz_carrete@yahoo.com>
2026-01-03 13:48:19 +0100

    works

--- test/Main.hs
+++ test/Main.hs
@@ -5,29 +5,31 @@
 module Main (main) where
 
 import GHC.Records
+import GHC.Generics
 import Optics.Core
+import Optics.Lens.Dot
 import GHC.TypeLits
 
 data Whole a = Whole {
     whole1 :: Int, 
     part :: Part a
-}
+} deriving stock Generic
 
 data Part a = Part {
     part1 :: Bool,
     subpart :: Subpart a  
-}
+} deriving stock Generic
 
 data Subpart a = Subpart {
     subpart1 :: String,
     foo :: a
-}
+} deriving stock Generic
 
 whole :: Whole Int
 whole = Whole 0 (Part True (Subpart "wee" 7))
 
--- whole' :: Whole Bool
--- whole' = whole .~  & False 
+whole' :: Whole Bool
+whole' = whole & the.part.subpart.foo .~ False 
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
