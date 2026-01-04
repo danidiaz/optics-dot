a99ddd8f8f346e279ec1ded0492b056eccf60e19
Daniel Díaz <diaz_carrete@yahoo.com>
2026-01-04 16:04:11 +0100

    still the fundep problem

--- lib/Optics/Dot.hs
+++ lib/Optics/Dot.hs
@@ -2,26 +2,60 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Optics.Dot where
 
 import Data.Kind
 import GHC.Records
 import Optics.Core
+import GHC.TypeLits
+import Data.Proxy
 
 instance
-  ( RecordDotOptics name u v a b,
+  ( 
+    HasOpticsMethod u,
+    RecordDotOptics (Method u) name u v a b,
     JoinKinds k A_Lens m,
     AppendIndices is NoIx ks
   ) =>
   HasField name (Optic k is s t u v) (Optic m ks s t a b)
   where
-  getField o = o % dotOptic @name
+  getField o = o % (dotOptic @(Method u) @name @u @v @a @b)
+
+
+class HasOpticsMethod s where
+  type Method s :: Type
 
 -- | 
--- The @name v -> u a b w@ fundep doesn't seem to be necessary.
-class RecordDotOptics name u v a b | name u -> v a b, name v -> u a b where
+-- The @name v -> u a b w@ fundep could be added but doesn't seem to be necessary.
+-- Could it improve inference?
+type RecordDotOptics :: Type -> Symbol -> Type -> Type -> Type -> Type -> Constraint
+class RecordDotOptics method name u v a b | name u -> a b, name v -> u a b where
   dotOptic :: Lens u v a b
+
+data GenericsDotOptics
+
+instance (HasOpticsMethod s, 
+          Method s ~ GenericsDotOptics, 
+          GField name s t a b) => 
+          RecordDotOptics GenericsDotOptics name s t a b where
+  dotOptic = gfield @name
+
+
+data FieldDotOptics
+
+instance
+  ( 
+    HasOpticsMethod a,
+    Method a ~ FieldDotOptics,
+    HasField name a b,
+    SetField name a b
+  ) =>
+  RecordDotOptics FieldDotOptics name a a b b
+  where
+  dotOptic = Optics.Core.lens (getField @name) (flip (setField @name))
+
 
 the :: Iso a b a b
 the = Optics.Core.equality
--- test/Main.hs
+++ test/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE NoFieldSelectors #-}
+{-# LANGUAGE TypeFamilies #-}
 
 -- https://stackoverflow.com/questions/53009549/haskell-derivingvia-on-multi-param-type-classes-with-fun-deps
 module Main (main) where
@@ -17,8 +18,8 @@ data Whole a = Whole
   }
   deriving stock (Generic, Show)
 
-instance (GField name (Whole p) (Whole q) a b) => RecordDotOptics name (Whole p) (Whole q) a b where
-  dotOptic = gfield @name
+instance HasOpticsMethod (Whole a) where
+  type Method (Whole a)  = GenericsDotOptics
 
 data Part a = Part
   { part1 :: Bool,
@@ -26,8 +27,8 @@ data Part a = Part
   }
   deriving stock (Generic, Show)
 
-instance (GField name (Part p) (Part q) a b) => RecordDotOptics name (Part p) (Part q) a b where
-  dotOptic = gfield @name
+instance HasOpticsMethod (Part a) where
+  type Method (Part a) = GenericsDotOptics
 
 data Subpart a = Subpart
   { wee :: String,
@@ -36,8 +37,8 @@ data Subpart a = Subpart
   }
   deriving stock (Generic, Show)
 
-instance (GField name (Subpart p) (Subpart q) a b) => RecordDotOptics name (Subpart p) (Subpart q) a b where
-  dotOptic = gfield @name
+instance HasOpticsMethod (Subpart a)  where
+  type Method (Subpart a) = GenericsDotOptics
 
 data YetAnotherSubpart = YetAnotherSubpart
   { ooo :: String,
@@ -48,13 +49,13 @@ data YetAnotherSubpart = YetAnotherSubpart
 -- | 'YetAnotherSubpart' doesn't use the 'GField' machinery for
 -- 'RecordDotOptics'. Instead, it uses 'HasField'/'SetField'. Field-changing
 -- updates are not supported here.
-instance
-  ( HasField name YetAnotherSubpart x,
-    SetField name YetAnotherSubpart x
-  ) =>
-  RecordDotOptics name YetAnotherSubpart YetAnotherSubpart x x
-  where
-  dotOptic = Optics.Core.lens (getField @name) (flip (setField @name))
+-- instance
+--   ( HasField name YetAnotherSubpart x,
+--     SetField name YetAnotherSubpart x
+--   ) =>
+--   RecordDotOptics name YetAnotherSubpart YetAnotherSubpart x x
+--   where
+--   dotOptic = Optics.Core.lens (getField @name) (flip (setField @name))
 
 instance SetField "ooo" YetAnotherSubpart String where
   setField ooo r = r {ooo}
@@ -62,20 +63,24 @@ instance SetField "ooo" YetAnotherSubpart String where
 whole :: Whole Int
 whole = Whole 0 (Part True (Subpart "wee" 7 (YetAnotherSubpart "oldval" 3)))
 
+
+whole'1 :: Whole Bool
+whole'1 = whole & the.part .~ (Part True (Subpart "wee" False (YetAnotherSubpart "oldval" 3)))
+
 -- | Note the the type-changing update
 whole' :: Whole Bool
-whole' = whole & the.part.subpart.foo .~ False
+whole' = whole & the.part.subpart .~ (Subpart "wee" False (YetAnotherSubpart "oldval" 3))
 
 -- | Non-type changed update which includes 'GField' lenses and 'HasField'/'SetField' lenses.
-whole'' :: Whole Int
-whole'' = whole & the.part.subpart.yet.ooo .~ "newval"
+-- whole'' :: Whole Int
+-- whole'' = whole & the.part.subpart.yet.ooo .~ "newval"
 
-normalDotAccess :: String
-normalDotAccess = whole.part.subpart.yet.ooo
+-- normalDotAccess :: String
+-- normalDotAccess = whole.part.subpart.yet.ooo
 
 main :: IO ()
 main = do
   print whole
   print whole'
-  print whole''
-  print normalDotAccess
+  -- print whole''
+  -- print normalDotAccess
